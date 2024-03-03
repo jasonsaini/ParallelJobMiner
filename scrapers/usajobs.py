e07c843d1d2a36ee4fdb79d0b660cf433f8a4d5c
Jason Saini <jasonsaini.exe@gmail.com>
2024-03-03 18:55:27 -0500

    added API retrieval for USAJOBS

--- scrapers/usajobs.py
+++ scrapers/usajobs.py
@@ -1,54 +1,94 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+from dotenv import load_dotenv
+#from .utils import HEADERS, Job
 
-from .utils import HEADERS, Job
+load_dotenv('token.env')
+api_key = os.getenv('USAJOBS_KEY')
 
-def scrape_usajobs(job_title: str):
-    uri = "https://www.usajobs.gov/search/results/?l=United States&k={}".format(job_title)
+# Manually read and print the contents of token.env
+with open('token.env') as file:
+    for line in file:
+        print(line.strip())
 
-    res = requests.get(uri, headers=HEADERS)
-    if res.status_code == 403:
-        print('USAJOBS: Denied. Counted as a bot')
+
+def scrape_usajobs(job_title, api_key):
+    base_url = "https://data.usajobs.gov/api/search"
+    headers = {
+        "User-Agent": "your_email@example.com",  # Replace with your email
+        "Authorization-Key": api_key
+    }
+    params = {
+        "Keyword": job_title
+    }
+
+    response = requests.get(base_url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        print("Failed to retrieve data from USAJobs")
         return
 
-    # Get the listings
-    res_text = BeautifulSoup(res.text, 'html.parser')
-    data = res_text.find('div', {'class': 'usajobs-search-results'})
-    listings = data.find_all('div', {'class': 'usajobs-search-result--core'})
+    data = response.json()
+    job_listings = data.get('SearchResult', {}).get('SearchResultItems', [])
+    print("USAJOBS Listings:")
+    for job in job_listings:
+        job_title = job['MatchedObjectDescriptor']['PositionTitle']
+        company_name = job['MatchedObjectDescriptor']['OrganizationName']
+        job_link = job['MatchedObjectDescriptor']['PositionURI']
 
-    job_list = []
+        print(f"Job Title: {job_title}\nCompany: {company_name}\nLink: {job_link}\n")
+ # Replace with your actual API key
+scrape_usajobs("Software Engineer", api_key)
 
-    for listing in listings:
-        job = Job()
+
+# def scrape_usajobs(job_title: str):
+#     uri = "https://www.usajobs.gov/search/results/?l=United States&k={}".format(job_title)
+
+#     res = requests.get(uri, headers=HEADERS)
+#     if res.status_code == 403:
+#         print('USAJOBS: Denied. Counted as a bot')
+#         return
+
+#     # Get the listings
+#     res_text = BeautifulSoup(res.text, 'html.parser')
+#     data = res_text.find('div', {'class': 'usajobs-search-results'})
+#     listings = data.find_all('div', {'class': 'usajobs-search-result--core'})
+
+#     job_list = []
+
+#     for listing in listings:
+#         job = Job()
         
-        # Company name
-        try:
-            job.name_of_company = (
-                listing
-                .find('div', {'class': 'usajobs-search-result--core__body'})
-                .find('div', {'class': 'usajobs-search-result--core__summary'})
-                .find('h4', {'class': 'usajobs-search-result--core__agency'})
-                .text
-                .strip()
-            )
-        except:
-            job.name_of_company = None
+#         # Company name
+#         try:
+#             job.name_of_company = (
+#                 listing
+#                 .find('div', {'class': 'usajobs-search-result--core__body'})
+#                 .find('div', {'class': 'usajobs-search-result--core__summary'})
+#                 .find('h4', {'class': 'usajobs-search-result--core__agency'})
+#                 .text
+#                 .strip()
+#             )
+#         except:
+#             job.name_of_company = None
 
-        # Salary
-        try:
-            job.salary = (
-                listing
-                .find('div', {'class': 'usajobs-search-result--core__body'})
-                .find('div', {'class': 'usajobs-search-result--core__details'})
-                .find('ul', {'class': 'usajobs-search-result--core__details-list'})
-                .find('li', {'class': 'usajobs-search-result--core__item'})
-                .text
-                .strip()
-            )
-        except:            
-            job.salary = None
+#         # Salary
+#         try:
+#             job.salary = (
+#                 listing
+#                 .find('div', {'class': 'usajobs-search-result--core__body'})
+#                 .find('div', {'class': 'usajobs-search-result--core__details'})
+#                 .find('ul', {'class': 'usajobs-search-result--core__details-list'})
+#                 .find('li', {'class': 'usajobs-search-result--core__item'})
+#                 .text
+#                 .strip()
+#             )
+#         except:            
+#             job.salary = None
 
-        # Add job to list
-        job_list.append(job)
+#         # Add job to list
+#         job_list.append(job)
 
-    print(f'List of jobs and their details for USAJOBS: {job_list}')
+#     print(f'List of jobs and their details for USAJOBS: {job_list}')
+
