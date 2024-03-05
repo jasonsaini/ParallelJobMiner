dee2098778b55fb127b49d7cf49e8c764010fde3
Joshua Byrd <jsbyrd02@gmail.com>
2024-03-05 14:33:54 -0500

    Try to add data to excel file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import multiprocessing as mp, time
 import argparse
+import threading
+import pandas as pd
+import os
 
 from scrapers import scrape_glassdoor
 from scrapers import scrape_indeed
@@ -7,18 +10,19 @@ from scrapers import scrape_linkedin
 from scrapers import scrape_monster
 from scrapers import scrape_usajobs
 
+
 # Starts the threads for scraping the sites
-def start_scrapers(job_title):
+def start_scrapers(job_title, data_frame):
     scrapers = {
-        'usajobs': scrape_usajobs,
-        'indeed': scrape_indeed,
-        'linkedin': scrape_linkedin,
-        'monster': scrape_monster,
-        'glassdoor': scrape_glassdoor,
+        'usajobs': scrape_usajobs
+        # 'indeed': scrape_indeed,
+        # 'linkedin': scrape_linkedin,
+        # 'monster': scrape_monster,
+        # 'glassdoor': scrape_glassdoor,
     }
 
     threads = [
-        mp.Process(target=scrapers[site], args=(job_title,))
+        mp.Process(target=scrapers[site], args=(job_title, data_frame))
         for site in scrapers
     ]
 
@@ -27,6 +31,7 @@ def start_scrapers(job_title):
 
     for thread in threads:
         thread.join()
+
 
 # Gets job title
 def get_job():
@@ -37,6 +42,38 @@ def get_job():
     return args.job_title
 
 
+class ThreadSafeDataframe:
+    excel_header = ["Site", "Job Title", "Company", "Link"]
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.index = 1
+        self.df = pd.DataFrame(columns=self.excel_header)
+
+    def print_df(self):
+        print(self.df)
+
+    def get_and_increment_index(self):
+        with self.lock:
+            self.index += 1
+            return self.index
+
+    def convert_df_to_excel(self):
+        print(self.df)
+        print(self.index)
+        exists = os.path.isfile('./jobs_output.xlsx')
+        if exists:
+            writer = pd.ExcelWriter('jobs_output.xlsx', mode='a', if_sheet_exists='replace')
+        else:
+            writer = pd.ExcelWriter('jobs_output.xlsx')
+        self.df.to_excel(writer, sheet_name='jobs_list')
+        writer.close()
+
+    def add_new_row(self, new_row, index):
+        print(f'{index}: {new_row}')
+        self.df.loc[index] = new_row
+
+
 if __name__ == "__main__":
     job_title = get_job()
 
@@ -44,7 +81,10 @@ if __name__ == "__main__":
 
     start_time = time.time()
 
-    start_scrapers(job_title)
+    data_frame = ThreadSafeDataframe()
+    print(hex(id(data_frame)))
+    start_scrapers(job_title, data_frame)
+    data_frame.convert_df_to_excel()
 
     elapsed_time = time.time() - start_time
     print(f'Search complete in {elapsed_time:.2f} seconds')
--- scrapers/usajobs.py
+++ scrapers/usajobs.py
@@ -7,7 +7,9 @@ from dotenv import load_dotenv
 load_dotenv('token.env')
 api_key = os.getenv('USAJOBS_KEY')
 
-def scrape_usajobs(job_title, api_key):
+
+def scrape_usajobs(job_title, data_frame):
+    print(hex(id(data_frame)))
     base_url = "https://data.usajobs.gov/api/search"
     headers = {
         "User-Agent": "your_email@example.com",  # Replace with your email
@@ -31,59 +33,9 @@ def scrape_usajobs(job_title, api_key):
         job_title = job['MatchedObjectDescriptor']['PositionTitle']
         company_name = job['MatchedObjectDescriptor']['OrganizationName']
         job_link = job['MatchedObjectDescriptor']['PositionURI']
-
-        print(f"Job Title: {job_title}\nCompany: {company_name}\nLink: {job_link}\n")
- # Replace with your actual API key
-scrape_usajobs("Software Engineer", api_key)
-
-
-# def scrape_usajobs(job_title: str):
-#     uri = "https://www.usajobs.gov/search/results/?l=United States&k={}".format(job_title)
-
-#     res = requests.get(uri, headers=HEADERS)
-#     if res.status_code == 403:
-#         print('USAJOBS: Denied. Counted as a bot')
-#         return
-
-#     # Get the listings
-#     res_text = BeautifulSoup(res.text, 'html.parser')
-#     data = res_text.find('div', {'class': 'usajobs-search-results'})
-#     listings = data.find_all('div', {'class': 'usajobs-search-result--core'})
-
-#     job_list = []
-
-#     for listing in listings:
-#         job = Job()
-        
-#         # Company name
-#         try:
-#             job.name_of_company = (
-#                 listing
-#                 .find('div', {'class': 'usajobs-search-result--core__body'})
-#                 .find('div', {'class': 'usajobs-search-result--core__summary'})
-#                 .find('h4', {'class': 'usajobs-search-result--core__agency'})
-#                 .text
-#                 .strip()
-#             )
-#         except:
-#             job.name_of_company = None
-
-#         # Salary
-#         try:
-#             job.salary = (
-#                 listing
-#                 .find('div', {'class': 'usajobs-search-result--core__body'})
-#                 .find('div', {'class': 'usajobs-search-result--core__details'})
-#                 .find('ul', {'class': 'usajobs-search-result--core__details-list'})
-#                 .find('li', {'class': 'usajobs-search-result--core__item'})
-#                 .text
-#                 .strip()
-#             )
-#         except:            
-#             job.salary = None
-
-#         # Add job to list
-#         job_list.append(job)
-
-#     print(f'List of jobs and their details for USAJOBS: {job_list}')
-
+        index = data_frame.get_and_increment_index()
+        new_row = ['USAJobs', job_title, company_name, job_link]
+        data_frame.add_new_row(new_row, index)
+        # print(f"Job Title: {job_title}\nCompany: {company_name}\nLink: {job_link}\n")
+    print(f'Finished Dataframe: {data_frame.df}')
+    return
